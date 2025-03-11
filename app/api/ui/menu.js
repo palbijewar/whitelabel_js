b860a661cb1c83e1e8b51e98c4beffa23cf3735d
palbijewar <pal.bijewar@gmail.com>
2025-03-11 17:38:09 +0530

    fixes: sidebar fixes

--- app/api/ui/menu.js
+++ app/api/ui/menu.js
@@ -3,6 +3,16 @@ module.exports = [
     key: 'home',
     name: 'Dashboard',
     icon: 'home',
+    link: '/app',
+    child: [
+      {
+        key: 'dashboard',
+        name: 'Dashboard',
+        link: '/app',
+        icon: 'business',
+        badge: 'Hot'
+      },
+    ]
   },
   {
     key: 'Marketplace',
@@ -13,12 +23,6 @@ module.exports = [
         key: 'static_apps',
         name: 'Group 1',
         title: true,
-      },
-      {
-        key: 'todo',
-        name: 'Option 1',
-        icon: 'check_circle',
-        link: '/app/pages/todo',
       },
       {
         key: 'contact',
@@ -62,16 +66,190 @@ module.exports = [
     key: 'Signals',
     name: 'Signals',
     icon: 'important_devices',
+    child: [
+      {
+        key: 'landing_page',
+        name: 'Landing Page',
+        title: true,
+      },
+      {
+        key: 'todo',
+        name: 'Option 1',
+        icon: 'check_circle',
+        link: '/app/pages/todo',
+      },
+    ]
   },
   {
     key: 'Subscription',
     name: 'Subscription',
     icon: 'Subscription',
+    child: [
+      {
+        key: 'indian-market',
+        name: 'Indian Market',
+        title: true,
+      },
+      {
+        key: 'banknifty',
+        name: 'BankNifty',
+        link: '/',
+        icon: 'business',
+        badge: 'Hot'
+      },
+      {
+        key: 'nifty',
+        name: 'Nifty',
+        link: '/',
+        icon: 'business',
+        badge: 'Hot'
+      },
+      {
+        key: 'crypto',
+        name: 'Crypto',
+        title: true,
+      },
+      {
+        key: 'crypto',
+        name: 'Cryptocurrency',
+        icon: 'local_atm',
+        link: '/app/dashboard/crypto'
+      },
+      {
+        key: 'forex',
+        name: 'Forex',
+        title: true,
+      },
+      {
+        key: 'usd',
+        name: 'USD',
+        icon: 'font_download',
+        link: '/app/ui/typography'
+      },
+      {
+        key: 'inr',
+        name: 'INR',
+        icon: 'bookmark',
+        link: '/app/ui/icons'
+      },
+    ]
   },
   {
     key: 'Notification',
     name: 'Notification',
     icon: 'border_color',
+    child: [
+      {
+        key: 'landing_page',
+        name: 'Landing Page',
+        title: true,
+      },
+      {
+        key: 'corporate',
+        name: 'Corporate',
+        link: '/',
+        icon: 'business',
+        badge: 'Hot'
+      },
+      {
+        key: 'dashboard',
+        name: 'Dashboard',
+        title: true,
+      },
+      {
+        key: 'personal',
+        name: 'Analytic',
+        icon: 'settings_brightness',
+        link: '/app'
+      },
+      {
+        key: 'crm',
+        name: 'Marketing',
+        icon: 'settings_system_daydream',
+        link: '/app/dashboard/marketing'
+      },
+      {
+        key: 'crypto',
+        name: 'Cryptocurrency',
+        icon: 'local_atm',
+        link: '/app/dashboard/crypto'
+      },
+      {
+        key: 'widgets',
+        name: 'Widgets',
+        title: true,
+      },
+      {
+        key: 'infographics_widget',
+        icon: 'timelapse',
+        name: 'Infographics',
+        link: '/app/widgets/infographics'
+      },
+      {
+        key: 'status_widget',
+        icon: 'nature_people',
+        name: 'Status',
+        link: '/app/widgets/status'
+      },
+      {
+        key: 'analytics_widget',
+        icon: 'insert_chart',
+        name: 'Analytics',
+        link: '/app/widgets/analytics'
+      },
+      {
+        key: 'mini_apps_widget',
+        icon: 'web',
+        name: 'Mini Apps',
+        link: '/app/widgets/mini-apps'
+      },
+      {
+        key: 'gallery_widget',
+        icon: 'satellite',
+        name: 'Gallery',
+        link: '/app/widgets/gallery-carousel'
+      },
+      {
+        key: 'material_font_icon',
+        name: 'Fonts & Icons',
+        title: true,
+      },
+      {
+        key: 'typography',
+        name: 'Typography',
+        icon: 'font_download',
+        link: '/app/ui/typography'
+      },
+      {
+        key: 'icons',
+        name: 'Material Icons',
+        icon: 'bookmark',
+        link: '/app/ui/icons'
+      },
+      {
+        key: 'layouts',
+        name: 'Layouts',
+        title: true,
+      },
+      {
+        key: 'grid',
+        name: 'Grid',
+        icon: 'view_column',
+        link: '/app/layouts/grid'
+      },
+      {
+        key: 'application_layout',
+        name: 'App Layout',
+        icon: 'web',
+        link: '/app/layouts/app-layout'
+      },
+      {
+        key: 'responsive',
+        name: 'Responsive',
+        icon: 'mobile_friendly',
+        link: '/app/layouts/responsive'
+      }
+    ]
   },
   {
     key: 'Api Connection',
--- app/containers/SampleApps/Todo/index.js
+++ app/containers/SampleApps/Todo/index.js
@@ -1,85 +1,50 @@
-import React, { useEffect } from 'react';
-import PropTypes from 'prop-types';
-import { useDispatch, useSelector } from 'react-redux';
-import brand from 'enl-api/dummy/brand';
-import { Helmet } from 'react-helmet';
-import {
-  TaskFilters,
-  TaskForm,
-  TaskList,
-  PapperBlock
-} from 'enl-components';
-import useStyles from 'enl-components/TodoList/todo-jss';
-import { injectIntl } from 'react-intl';
-import messages from 'enl-components/TodoList/messages';
-import {
-  createTaskAction, filterTasksAction, removeTaskAction,
-  updateTaskAction, fetchTasksAction
-} from './reducers/todoSlice';
-import data from './api/todoData';
+import React from 'react';
+import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper, Typography } from '@mui/material';
 
-function Todo(props) {
-  const { intl } = props;
-  const { classes } = useStyles();
-  const title = brand.name + ' - Todo App';
-  const description = brand.desc;
+const algoSignals = [
+  { id: 1, algo: 'BULLBEAR NIFTY INTRADAY', script: 'NIFTY13MAR2522450CE', price: '', signal: 'SELL', time: '2025-03-11 10:34:03' },
+  { id: 2, algo: 'BULL BEAR BANKNIFTY INTRADAY', script: 'BANKNIFTY27MAR2547900CE', price: '', signal: 'SELL', time: '2025-03-11 10:23:08' },
+  { id: 3, algo: 'BULL BEAR BANKNIFTY INTRADAY', script: 'BANKNIFTY27MAR2547900CE', price: '', signal: 'BUY', time: '2025-03-11 10:20:14' },
+  { id: 4, algo: 'BULLBEAR NIFTY INTRADAY', script: 'NIFTY13MAR2522450CE', price: '', signal: 'BUY', time: '2025-03-11 10:15:10' },
+  { id: 5, algo: 'BULL BEAR BANKNIFTY INTRADAY', script: 'BANKNIFTY27MAR2547900PE', price: '', signal: 'SELL', time: '2025-03-11 09:30:28' }
+];
 
-  // Redux State
-  const taskList = useSelector(state => state.todo.list);
-  const filterType = useSelector(state => state.todo.filter);
-  const filteredTasks = () => {
-    switch (filterType) {
-      case 'active':
-        return taskList.filter(task => !task.completed);
-      case 'completed':
-        return taskList.filter(task => task.completed);
-      default:
-        return taskList;
-    }
-  };
-
-  // Dispatcher
-  const dispatch = useDispatch();
-
-  useEffect(() => {
-    dispatch(fetchTasksAction(data));
-  }, []);
-
+function AlgoSignalsTable() {
   return (
-    <div>
-      <Helmet>
-        <title>{title}</title>
-        <meta name="description" content={description} />
-        <meta property="og:title" content={title} />
-        <meta property="og:description" content={description} />
-        <meta property="twitter:title" content={title} />
-        <meta property="twitter:description" content={description} />
-      </Helmet>
-      <PapperBlock
-        title={intl.formatMessage(messages.title)}
-        icon="playlist_add_check"
-        noMargin
-        whiteBg
-        colorMode="light"
-        desc={intl.formatMessage(messages.subtitle)}
-        className={classes.root}
-      >
-        <TaskForm handleSubmit={(payload) => dispatch(createTaskAction(payload))} />
-        <div className="g-col">
-          <TaskFilters filter={(payload) => dispatch(filterTasksAction(payload))} type={filterType} />
-          <TaskList
-            removeTask={(payload) => dispatch(removeTaskAction(payload))}
-            tasks={filteredTasks()}
-            updateTask={(task, change) => dispatch(updateTaskAction({ task, change }))}
-          />
-        </div>
-      </PapperBlock>
-    </div>
+    <Paper sx={{ padding: 2, margin: 2 }}>
+      <Typography variant="h6" align="center" gutterBottom>
+        Algo Signal's
+      </Typography>
+      <TableContainer component={Paper}>
+        <Table>
+          <TableHead>
+            <TableRow>
+              <TableCell>No</TableCell>
+              <TableCell>Algo</TableCell>
+              <TableCell>Script</TableCell>
+              <TableCell>Price</TableCell>
+              <TableCell>Signal</TableCell>
+              <TableCell>Time</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {algoSignals.map((row) => (
+              <TableRow key={row.id}>
+                <TableCell>{row.id}</TableCell>
+                <TableCell>{row.algo}</TableCell>
+                <TableCell>{row.script}</TableCell>
+                <TableCell>{row.price || '-'}</TableCell>
+                <TableCell style={{ color: row.signal === 'BUY' ? 'green' : 'red', fontWeight: 'bold' }}>
+                  {row.signal}
+                </TableCell>
+                <TableCell>{row.time}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </Paper>
   );
 }
 
-Todo.propTypes = {
-  intl: PropTypes.object.isRequired
-};
-
-export default injectIntl(Todo);
+export default AlgoSignalsTable;
